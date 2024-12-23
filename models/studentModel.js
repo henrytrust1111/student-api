c1fd21a6257b6b719fd5e5a699c0772172ca0ea5
henrytrust1111 <henrytrust1111@gmail.com>
2024-12-23 06:01:36 +0100

    first commit

---
+++ models/studentModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    age: {
+      type: Number,
+      required: [true, 'Age is required'],
+    },
+    grade: {
+      type: String,
+      required: [true, 'Grade is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Student = mongoose.model('Student', studentSchema);
+module.exports = Student;
